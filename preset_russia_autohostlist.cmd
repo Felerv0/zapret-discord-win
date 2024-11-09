df409ac8d0c10d01f22fe7d8612005cff5685438
BADtochka <badmodertwitch@gmail.com>
2024-11-09 03:44:20 +0300

    fix: service

--- preset_russia_autohostlist.cmd
+++ preset_russia_autohostlist.cmd
@@ -1,7 +1,6 @@
 SET WINWSPATH=%~dp0\zapret-winws\
 
-start "zapret: http,https,quic" /min "%WINWSPATH%winws.exe" ^
---wf-tcp=80,443 --wf-udp=443,50000-50099 ^
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50099 ^
 --filter-udp=50000-50099 --ipset="%WINWSPATH%ipset-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n2 --new ^
 --filter-udp=50000-50099 --new ^
 --filter-udp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%WINWSPATH%quic_initial_www_google_com.bin" --new ^
@@ -9,3 +8,5 @@ start "zapret: http,https,quic" /min "%WINWSPATH%winws.exe" ^
 --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt" --new ^
 --filter-tcp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%WINWSPATH%tls_clienthello_www_google_com.bin" --new ^
 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt"
+
+start "zapret: http,https,quic" /min "%WINWSPATH%winws.exe" %ARGS%
--- service_create.cmd
+++ service_create.cmd
@@ -1,18 +1,18 @@
 SET WINWSPATH=%~dp0\zapret-winws\
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---new --filter-udp=50000-50099 --ipset="%WINWSPATH%ipset-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n2 ^
---new --filter-udp=50000-50099 ^
---new --filter-udp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%WINWSPATH%quic_initial_www_google_com.bin" ^
---new --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 ^
---new --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt" ^
---new --filter-tcp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%WINWSPATH%tls_clienthello_www_google_com.bin" ^
---new --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt"
+--filter-udp=50000-50099 --ipset="%WINWSPATH%ipset-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n2 --new ^
+--filter-udp=50000-50099 --new ^
+--filter-udp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%WINWSPATH%quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt" --new ^
+--filter-tcp=443 --hostlist="%WINWSPATH%list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%WINWSPATH%tls_clienthello_www_google_com.bin" --new ^
+--dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%WINWSPATH%autohostlist.txt"
 call :srvinst winws1
 goto :eof
 
 :srvinst
 net stop %1
 sc delete %1
-sc create %1 start= demand displayname= "zapret DPI bypass : %1" binpath= "%WINWSPATH%winws.exe %ARGS%"
+sc create %1 start= auto displayname= "zapret DPI bypass : %1" binpath= "%WINWSPATH%winws.exe %ARGS%"
 sc description %1 "zapret DPI bypass software"
 sc start %1
